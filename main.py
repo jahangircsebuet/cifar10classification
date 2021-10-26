8be30000de8ad3b20f445bb427ca83ab74fa1405
Jahangir <jahangir.08.cse@gmail.com>
2021-10-27 02:51:28 +0600

    apply arguments, store train accuracy, test accuracy, loss

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ from CIFAR10DataLoader import CIFAR10DataLoader
 import argparse
 
 # arguments
-useCuda = 'Y'
-data_path = './data'
-workers = 2
-optimizer = 'sgd'
-is_gpu = False
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--useCuda', default='Y', type=str, help='use cuda or not')
 parser.add_argument('--dataPath', default='./data', type=str, help='data path where data to be stored')
@@ -37,9 +32,9 @@ epochs = 5
 print("init data loader class")
 data_loader = CIFAR10DataLoader()
 print("load data")
-train, train_loader, test, test_loader = data_loader.load(root=data_path, transform=transform_config,
+train, train_loader, test, test_loader = data_loader.load(root=args.dataPath, transform=transform_config,
                                                           train_batch_size=train_batch,
-                                                          test_batch_size=test_batch, num_workers=workers)
+                                                          test_batch_size=test_batch, num_workers=args.workers)
 print("data loaded successfully")
 
 # Model
@@ -56,58 +51,89 @@ correct = 0
 total = 0
 loss_fn = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+train_losses = []
+train_accuracies = []
+best_training_accuracy = 0
 
-for i, data in enumerate(train_loader):
-    # print("i: ", i)
-    # get the inputs
-    inputs, labels = data[0], data[1]
-    # print(inputs.shape)
-    device = torch.device("cuda:0" if torch.cuda.is_available else "cpu")
-    # inputs, labels = data[0].to(device), data[1].to(device)
-    if is_gpu:
-        inputs = inputs.cuda()
-        labels = labels.cuda()
-        net.to(device)
+# train model for epochs times, store training losses, training accuracies
+for epoch in range(epochs):
+    for i, data in enumerate(train_loader):
+        # get the inputs
+        inputs, labels = data[0], data[1]
+        # print(inputs.shape)
+        device = torch.device("cuda:0" if torch.cuda.is_available else "cpu")
+        # inputs, labels = data[0].to(device), data[1].to(device)
+        if args.useCuda:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+            net.to(device)
 
-    #     # wrap them in Variable
-    #     inputs, labels = Variable(inputs), Variable(labels)
+        #     wrap them in Variable
+        #     inputs, labels = Variable(inputs), Variable(labels)
 
-    # zero the parameter gradients
-    optimizer.zero_grad()
-    #
-    #     # forward + backward + optimize
-    outputs = net.forward(inputs)
-    loss = loss_fn(outputs, labels)
-    loss.backward()
-    optimizer.step()
-    #
-    # print statistics
-    running_loss += loss.item()
-    # print("running_loss: ", running_loss)
+        # zero the parameter gradients
+        optimizer.zero_grad()
 
-    _, predicted = outputs.max(1)
-    total += labels.size(0)
-    print("total: ", total)
-    correct += predicted.eq(labels).sum().item()
-    print("correct: ", correct)
-    #
-    # Normalizing the loss by the total number of train batches
-    running_loss /= len(train_loader)
-    # accuracy
-    train_accuracy = 100 * correct / total
-    print("train_accuracy: ", train_accuracy)
-#
-#     print('Epoch {}, train Loss: {:.3f}'.format(epochs, loss.item()), "Training Accuracy: %d %%" % train_accuracy)
+        # forward + backward + optimize
+        outputs = net.forward(inputs)
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+        optimizer.step()
 
-PATH = './cifar_net.pth'
-torch.save(net.state_dict(), PATH)
+        running_loss += loss.item()
+
+        _, predicted = outputs.max(1)
+        total += labels.size(0)
+        print("total: ", total)
+        correct += predicted.eq(labels).sum().item()
+        print("correct: ", correct)
+        #
+        # Normalizing the loss by the total number of train batches
+        running_loss /= len(train_loader)
+        train_losses.append(running_loss)
+        # accuracy
+        train_accuracy = 100 * correct / total
+        print("train_accuracy: ", train_accuracy)
+        train_accuracies.append(train_accuracy)
+
+        if train_accuracy > best_training_accuracy:
+            best_training_accuracy = train_accuracy
+            PATH = './cifar_net.pth'
+            torch.save({
+                'model': net.state_dict()
+            }, PATH)
 print('==> Finished Training ...')
-
-# dataiter = iter(test_loader)
-# images, labels = dataiter.next()
+print("losses")
+print(train_losses)
+print("train_accuracies")
+print(train_accuracies)
+print("best_training_accuracy: ", best_training_accuracy)
 
 net = CNN()
-net.load_state_dict(torch.load(PATH))
+stored_result = torch.load(PATH)
+# load model
+net.load_state_dict(stored_result['model'])
+
+# prediction on training dataset
+correct = 0
+total = 0
+best_training_accuracy = 0
+with torch.no_grad():
+    for i, data in enumerate(train_loader):
+        images, labels = data[0], data[1]
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total = labels.size(0)
+        print("train.total: ", total)
+        correct = (predicted == labels).sum().item()
+        print("train.correct: ", correct)
+
+        train_accuracy = 100 * correct / total
+        if train_accuracy > best_training_accuracy:
+            best_training_accuracy = train_accuracy
+print("best_training_accuracy: ", best_training_accuracy)
 
 # Test whole dataset
 print("############Test whole dataset##########")
@@ -122,11 +148,8 @@ with torch.no_grad():
         # the class with the highest energy is what we choose as prediction
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
-        print("test.total: ", total)
         correct += (predicted == labels).sum().item()
-        print("test.correct: ", correct)
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (
+print('Test accuracy(10000 images): %d %%' % (
         100 * correct / total))
-
 print('==> Finished Testing ...')
